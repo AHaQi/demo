f16033a52f65964d6f1828055462f2369babeed8
GitHub Action <action@github.com>
2024-11-05 07:45:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/ahaqi/demo/MyBundle.kt
+++ src/main/kotlin/com/github/ahaqi/demo/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ahaqi.demo
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ahaqi/demo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ahaqi/demo/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ahaqi.demo.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ahaqi.demo.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
